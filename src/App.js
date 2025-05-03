9a9f22b0ef599983d1a8f3641c1bccb6cc8b39a2
seanafella <57677802+seanafella@users.noreply.github.com>
2025-05-03 17:39:24 +0200

    TeacherToolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
 
-function App() {
+const module = [
+  { title: "Exercise Instructions" },
+  {
+    title: "Countdown Timer",
+  },
+
+  { title: "Translation" },
+  { title: "Exit Ticket" },
+];
+
+export default function App() {
+  const [button, setButton] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header />
+      <Buttons buttons={module} />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  const [objective, setObjective] = useState("");
+  return (
+    <div className="header">
+      <h1>Teacher Toolkit</h1>
+      <h3>Click on the module you need below</h3>
+      <span className="">
+        Todays Objective:
+        <input
+          className="input-text"
+          type="text"
+          placeholder="e.g To understand how variables work in programming"
+          onChange={(e) => setObjective(e.target.value)}
+        ></input>
+      </span>
+    </div>
+  );
+}
+
+function body() {
+  return;
+  <ExerciseInstructions />;
+}
+
+function Buttons({ buttons }) {
+  return (
+    <div className="nav-container">
+      {buttons.map((el, i) => (
+        <Button title={el.title} key={i}></Button>
+      ))}
+    </div>
+  );
+}
+
+function ExerciseInstructions() {
+  const [instructions, setInstructions] = useState("");
+
+  return (
+    <div>
+      <p>What would you like your first exercise to be?</p>
+      <input type="text"></input>
+      <button>add another exercise here</button>
+      <button>Show in order</button>;
+    </div>
+  );
+}
+
+function CountdownTimer() {}
+
+function Translation() {}
+
+function ExitTicket() {}
+
+function Button({ title }) {
+  return (
+    <div>
+      <button className="teacher-button">{title}</button>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      Designed by{" "}
+      <a href="https://www.seandavisproductions.co.uk">
+        Sean Davis Productions
+      </a>{" "}
+      using React
+    </div>
+  );
+}
